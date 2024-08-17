64b6f66e3bda1bb0c1fa9a8bd163ffc0d2397ed0
John Culbertson <john.a.culbertson@gmail.com>
2024-08-17 14:23:22 -0400

    Working version of 1b

--- 1/day1.go
+++ 1/day1.go
@@ -9,8 +9,22 @@ import (
 )
 
 func main() {
+	words := [10]string{
+		"zero",
+		"one",
+		"two",
+		"three",
+		"four",
+		"five",
+		"six",
+		"seven",
+		"eight",
+		"nine",
+	}
+
 	fmt.Println("Starting from the beginning.")
 	f, err := os.Open("day1a.text")
+	//	f, err := os.Open("day1a.text")
 
 	if err != nil {
 		fmt.Println("No file found.")
@@ -19,15 +33,42 @@ func main() {
 	total := 0
 	for fs.Scan() {
 		s := fs.Text()
+
+		earliest := len(s)
+		latest := 0
+		firstDigit := 0
+		lastDigit := 0
+
 		fmt.Printf("\nToken: %q\n", s)
 		pattern := "0123456789"
-		first := strings.IndexAny(s, pattern)
-		firstNum, _ := strconv.Atoi(s[first : first+1])
-		last := strings.LastIndexAny(s, pattern)
-		lastNum, _ := strconv.Atoi(s[last : last+1])
-		fmt.Printf("%d (%d) and %d (%d)", first, firstNum, last, lastNum)
-		total += (firstNum * 10) + lastNum
-		fmt.Printf("\n\n%d\n\n", total)
+		if first := strings.IndexAny(s, pattern); first != -1 {
+			earliest = first
+			firstDigit, _ = strconv.Atoi(s[first : first+1])
+		}
+		if last := strings.LastIndexAny(s, pattern); last != -1 {
+			latest = last
+			lastDigit, _ = strconv.Atoi(s[last : last+1])
+		}
+		// Look for string versions of numbers.
+
+		for i := 0; i < len(words); i++ {
+			if early := strings.Index(s, words[i]); early != -1 {
+				if early < earliest {
+					earliest = early
+					firstDigit = i
+				}
+			}
+			if late := strings.LastIndex(s, words[i]); late != -1 {
+				if late > latest {
+					latest = late
+					lastDigit = i
+				}
+			}
+		}
+		fmt.Printf("%d (%d) and %d (%d)", earliest, firstDigit, latest, lastDigit)
+		total += (firstDigit * 10) + lastDigit
+		fmt.Printf("\n%d\n", total)
 	}
+
 	fmt.Printf("\n\n%d\n\n", total)
 }
